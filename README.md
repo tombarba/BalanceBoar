d508d7d3b7035d6e5386f2e41f8e2ce33581609e
Tommaso Barbariol <tommaso.barbariol@studenti.unipd.it>
2024-08-25 18:53:51 +0200

    cleaning

--- README.md
+++
@@ -1,3 +0,0 @@
-# BalanceBoard
-
-This repo to keep track of the balance board proj.
--- window_quit_test.py
+++
@@ -1,8 +0,0 @@
-import tkinter as tk
-
-def quit(root):
-    root.destroy()
-
-root = tk.Tk()
-tk.Button(root, text="Quit", command=lambda root=root:quit(root)).pack()
-root.mainloop()
